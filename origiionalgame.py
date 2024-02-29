168152517475f141e8fef84fbb06410b0707801f
aiyza-junaid <aiyza.junaid@gmail.com>
2024-02-29 22:08:12 +0500

    bfs, aliens spawnedd

--- origiionalgame.py
+++ origiionalgame.py
@@ -1,3 +1,4 @@
+from collections import deque
 import random
 
 class Node:
@@ -9,6 +10,38 @@ class Node:
         self.alien = alien
         self.civilian = civilian
 
+def spawn_aliens_bfs(graph, source):
+    
+    frontier = deque([source])
+    visited_nodes = set()
+    all_paths = []
+
+
+    while frontier:
+        current_node = frontier.popleft()
+
+        #randomly generate aliens in each city 
+        current_node.alien = random.randint(100, 500)
+
+        print(f"Node: {current_node.city}, Random Aliens spawned: {current_node.alien}")
+
+        visited_nodes.add(current_node.city)
+
+        #print(f"Visited: {visited_nodes}")
+
+        for action in current_node.actions:
+            child = Node(action[0], current_node, graph[action[0]].actions, graph[action[0]].total_cost, graph[action[0]].alien, graph[action[0]].civilian)
+
+            if child.city not in visited_nodes and not any(n.city == child.city for n in frontier):
+                #print(child.city)
+                
+                frontier.append(child)
+                #print(f"Frontier: {[n.city for n in frontier]}")
+                
+   
+    return all_paths
+
+
 graph ={'A': Node('A', None, [('B',4),('C',2),('D',3)], 0, 0, 100),
         'B': Node('B', None, [('E',5),('A',4)], 0, 0, 300),
         'C': Node('C', None, [('A',2),('D',1),('Alexendria',5)], 0, 0, 600),
@@ -17,3 +50,8 @@ graph ={'A': Node('A', None, [('B',4),('C',2),('D',3)], 0, 0, 100),
         'F': Node('F', None, [('E',7),('G',2)], 0, 0, 800),
         'G': Node('G', None, [('Alexendria',2),('F',2)], 0, 0, 700),
         'Alexendria': Node('Alexendria', None, [('C',5),('G',2)], 0, 0, 400)}
+
+source_node = Node(graph['A'].city, None, graph['A'].actions, 0, 0, 0)
+
+spawn_aliens_bfs(graph,source_node)
+
